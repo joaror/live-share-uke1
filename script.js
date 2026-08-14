5c73304acfde658841ef311c251bb11a3072727d
joaror <joakim.rorstad@gmail.com>
2026-08-14 14:25:19 +0200

    Utvikling

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
-// Vi har lagt til 2 knapper i index.html, på toppen av dokumentet
-// de har hver sin onclick attribute som peker til to forskjellige funksjoner
-// changeBgColor() og changeLayout()
-// det betyr at når du klikker på en av disse knappene, så kjøres funksjonen som er spesifisert i html
-
 // funksjon som endrer DIV som encapsulater all content på siden
-// vi valgte å toggle class mellom row og column
-// i HTML så har id="page" en class som heter "row"
-// i funksjonen så toggles så row av, siden den er på
-// og class "column" toggles på, siden den ikke er der fra før
 function changeLayout() {
     document.getElementById("page").classList.toggle("row");
     document.getElementById("page").classList.toggle("column");
@@ -15,8 +6,6 @@ function changeLayout() {
     console.log("hej");
    }
 
-// her setter vi en variable som er true by default
-// når siden refreshes så kommer denne til å være "true"
 let toggle = true; 
 
 /* 
@@ -38,12 +27,115 @@ function changeBgColor() {
        
    }
 
-   // når funksjonen har gått gjennom if/else statements så kjøres denne koden
-   // denne koden endrer variabelen "toggle" til false
-   // utropstegn (!) er det samme som "not" eller false
    // gjør variable "toggle" til "false"
    toggle = !toggle;
-/* 
-toggle ? bgColor.style.backgroundColor = "pink" : ;
- */
+   
 }
+
+let noe = "snart helg"
+
+let isJoakim = true;
+let joakimId = document.getElementById("Joakim")
+let test = document.querySelector(".joakim")
+
+let input = document.getElementById("input-test")
+console.log(input.target.value)
+console.log(test)
+console.log(joakimId)
+function toggleJoakim() {
+   isJoakim ? joakimId.innerHTML = /* HTML */ '<div class="text column text-center">show me</div>' :
+      joakimId.innerHTML = `
+            <h1>${test}</h1>
+            <h1 class="text">Joakim Rørstad</h1>
+            <h2 class="text">Tønsberg</h2>
+            <ul class="text">
+               <li>Musikk</li>
+               <li>IT</li>
+               <li>Friluft</li>
+            </ul>
+         `
+         
+   isJoakim = !isJoakim;
+}
+
+toggleJoakim();
+
+let stateMartinius = false
+
+function toggleMartinius() {
+   if (stateMartinius === true) {
+      hideMartinius()
+      stateMartinius = false
+   }
+   else if (stateMartinius === false) {
+       showMartinius()
+      stateMartinius = true
+   }
+}
+
+function hideMartinius() {
+ document.getElementById("martinius").innerHTML = `<h1 class="text-center">Show Martinius</h1>`
+}
+
+function showMartinius() {
+   document.getElementById("martinius").innerHTML = `
+         <div class="column align-center text-center gap-2">
+            <h1>Martinius Karlsen</h1>
+            <h2>Drammen</h2>
+         </div>
+         <div>
+            <ul>
+               <li>Programmering</li>
+               <li>Simulator spill</li>
+               <li>Problemløsning</li>
+               <li>Aliens & UFO's</li>
+               <li>Parapsykologi</li>
+               <li>Self-hosting</li>
+            </ul>
+         </div>`
+}
+toggleMartinius()
+
+function showCato() {
+   document.getElementById("cato").innerHTML = `<h1>Cato Lindanger</h1>
+    <h2>Rennesøy, Stavanger</h2>
+    <ul>
+        <li>Skyting</li>
+        <li>Gaming</li>
+        <li>Gitar</li>
+        <li>Kjøring</li>
+    </ul>`
+}
+
+function hideCato() {
+   document.getElementById("cato").innerHTML = ``
+}
+
+
+
+
+
+
+
+
+
+
+
+/* functio toggle */
+
+
+function toggleLavinia() {
+   let lavinia = document.getElementById("lavinia")
+   if (stateLavinia === true) {
+      hideLavinia()
+      stateLavinia = false}
+      else if (stateLavinia === false) {
+      showLavinia()
+      stateLavinia = true}
+      function hideLavinia() { 
+         document.getElementById("lavinia").innerHTML = `<h1 class="text-center">Show Lavinia</h1>`
+      }
+      function showLavinia()
+      {}
+
+   }
---
+++ tiljesse.js
@@ -0,0 +1,57 @@
+// Vi har lagt til 2 knapper i index.html, på toppen av dokumentet
+// de har hver sin onclick attribute som peker til to forskjellige funksjoner
+// changeBgColor() og changeLayout()
+// det betyr at når du klikker på en av disse knappene, så kjøres funksjonen som er spesifisert i html
+
+// funksjon som endrer DIV som encapsulater all content på siden
+// vi valgte å toggle class mellom row og column
+// i HTML så har id="page" en class som heter "row"
+// i funksjonen så toggles så row av, siden den er på
+// og class "column" toggles på, siden den ikke er der fra før
+function changeLayout() {
+    document.getElementById("page").classList.toggle("row");
+    document.getElementById("page").classList.toggle("column");
+   
+    console.log("hej");
+   }
+
+// her setter vi en variable som er true by default
+// når siden refreshes så kommer denne til å være "true"
+let toggle = true; 
+
+/* 
+let bgColor = document.querySelector("body");*/
+
+function changeBgColor() {
+   // vi bruker en if/else statement for å sjekke om variable "toggle" er
+   // true eller false
+   if (toggle) {
+      // om toggle er true så kjøres denne koden, og bakgrunnen endres til rosa
+        document.querySelector("body").style.backgroundColor = "pink";
+   }
+   else {
+      // om toggle er false så kjøres denne koden og bakgrunnen endres til grå
+        document.querySelector("body").style.backgroundColor = "#93b438"  
+
+        
+        
+       
+   }
+
+   // når funksjonen har gått gjennom if/else statements så kjøres denne koden
+   // denne koden endrer variabelen "toggle" til false
+   // utropstegn (!) er det samme som "not" eller false
+   // gjør variable "toggle" til "false"
+   toggle = !toggle;
+/* 
+toggle ? bgColor.style.backgroundColor = "pink" : ;
+ */
+}
+
+
+function showJoakim() {
+   document.getElementById("Joakim").innerText = ""
+   showJoakim();
+}
+
+
